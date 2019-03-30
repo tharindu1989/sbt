888ed4268614078218af57cace12ac1718611627
tharindu1989 <tharindupro89@gmail.com>
2019-03-30 13:15:07 +0800

    ddd

---
+++ server/src/main/java/com/uob/bts2/sharedservices/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.uob.bts2.sharedservices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+import java.util.Arrays;
+
+//@EnableGlobalMethodSecurity
+@Configuration
+public class SecurityConfiguration{
+
+    
+}
+
+
